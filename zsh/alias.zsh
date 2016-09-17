bc488346d92fc5b50b0e1f8a6d058d63be65fd1b
rm <jonas@webbish.se>
2016-09-17 18:30:44 +0200

    starting fresh

---
+++ zsh/alias.zsh
@@ -0,0 +1,11 @@
+alias zc="vim ~/.zshrc"
+alias brewski='brew update && brew upgrade --all && brew upgrade brew-cask; brew cleanup; brew cask cleanup; brew doctor'
+
+# Laravel specific aliases
+alias art="php artisan"
+alias arl="php artisan route:list"
+alias arc="php artisan route:clear"
+alias artm="php artisan make"
+alias cda="composer dump-autoload"
+alias gulpack='gulp webpack && echo "Last run: " && date'
+alias art="php artisan"
